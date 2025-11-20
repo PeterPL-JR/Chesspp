6c7eb7a844cfc0c00f4e191da6423fa6b25f43e0
peterpljr <piokaf05@gmail.com>
2025-11-20 14:17:56 +0100

    2D array of pieces added

--- chess/Chessboard.cpp
+++ chess/Chessboard.cpp
@@ -1,5 +1,7 @@
 #include "Chessboard.h"
 
+#include "Piece.h"
+
 Field Chessboard::LIGHT_FIELD(sf::Color(227, 195, 135), sf::Color(160, 137, 94));
 Field Chessboard::DARK_FIELD(sf::Color(101, 51, 24), sf::Color(53, 28, 14));
 
@@ -35,6 +37,15 @@ void Chessboard::draw(int x, int y, Window* window) {
     }
 }
 
+Piece* Chessboard::get_piece(int x, int y) {
+    if (!is_field_valid(x, y)) return nullptr;
+    return board[x][y];
+}
+
+void Chessboard::set_piece(int x, int y, Piece *piece) {
+    if (!is_field_valid(x, y)) return;
+    board[x][y] = piece;
+}
 
 void Chessboard::draw_piece(Piece& piece, int x_offset, int y_offset, Window* window) {
     int x = piece.get_x() * Field::size + x_offset;
@@ -44,7 +55,17 @@ void Chessboard::draw_piece(Piece& piece, int x_offset, int y_offset, Window* wi
 
 void Chessboard::init_pieces(Piece::Colour colour, int pieces_y, int pawns_y) {
     for (int i = 0; i < SIZE; i++) {
-        pieces.push_back(Piece(PIECES_ORDER[i], colour, i, pieces_y));
-        pieces.push_back(Piece(Piece::PAWN, colour, i, pawns_y));
+        add_new_piece(PIECES_ORDER[i], colour, i, pieces_y);
+        add_new_piece(Piece::PAWN, colour, i, pawns_y);
     }
+}
+
+void Chessboard::add_new_piece(Piece::Type type, Piece::Colour colour, int x, int y) {
+    Piece* piece = new Piece(type, colour, x, y);
+    pieces.push_back(*piece);
+    set_piece(x, y, piece);
+}
+
+bool Chessboard::is_field_valid(int x, int y) {
+    return x >= 0 && y >= 0 && x < SIZE && y < SIZE;
 }
--- chess/Chessboard.h
+++ chess/Chessboard.h
@@ -15,12 +15,20 @@ public:
 
     void draw(int x, int y, Window* window);
 
+    Piece* get_piece(int x, int y);
 private:
+    Piece* board[SIZE][SIZE] = {};
     std::vector<Piece> pieces;
+
+    void set_piece(int x, int y, Piece* piece);
 
     void draw_piece(Piece& piece, int x_offset, int y_offset, Window* window);
 
     void init_pieces(Piece::Colour colour, int pieces_y, int pawns_y);
+
+    void add_new_piece(Piece::Type type, Piece::Colour colour, int x, int y);
+
+    bool is_field_valid(int x, int y);
 };
 
 #endif
