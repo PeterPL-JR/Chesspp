711d14a5e52da35b9d507c8ecb9b9c50eb818617
PeterPL-JR <piokaf05@gmail.com>
2026-01-27 22:44:11 +0100

    Text class

---
+++ graphics/drawable/Text.cpp
@@ -0,0 +1,69 @@
+#include "Text.h"
+
+Text::Text(std::string text, sf::Font& font, int size) : Text(font, size) {
+    set_text(text);
+}
+
+Text::Text(sf::Font& font, int size) : size(size) {
+    text_sf = sf::Text();
+    text_sf.setFont(font);
+    text_sf.setCharacterSize(size);
+    set_alignment(LEFT);
+}
+
+void Text::set_text(std::string text) {
+    this->text = text;
+    text_sf.setString(text);
+    update_text_position();
+}
+
+std::string Text::get_text() {
+    return text;
+}
+
+sf::Font Text::get_font() {
+    return font;
+}
+
+int Text::get_size() {
+    return size;
+}
+
+void Text::set_colour(Colour colour) {
+    text_sf.setFillColor(colour.get());
+}
+
+void Text::set_border(int size, Colour colour) {
+    text_sf.setOutlineColor(colour.get());
+    text_sf.setOutlineThickness(size);
+}
+
+void Text::set_alignment(Alignment alignment) {
+    this->alignment = alignment;
+    update_text_position();
+}
+
+void Text::draw(float x, float y, Window *window) {
+    text_sf.setPosition(x + render_offset_x, y);
+    window->draw(&text_sf);
+}
+
+void Text::update_text_position() {
+    if (alignment == LEFT) {
+        render_offset_x = 0;
+    } else {
+        float text_width = text_sf.getLocalBounds().width;
+        if (alignment == CENTER) {
+            render_offset_x = -text_width / 2;
+        }
+        if (alignment == RIGHT) {
+            render_offset_x = -text_width;
+        }
+    }
+}
+
+sf::Font Text::load_font(std::string location) {
+    sf::Font font;
+    font.loadFromFile("font/" + location + ".ttf");
+    return font;
+}
---
+++ graphics/drawable/Text.h
@@ -0,0 +1,43 @@
+#ifndef CHESSPP_TEXT_H
+#define CHESSPP_TEXT_H
+
+#include <SFML/Graphics/Font.hpp>
+
+#include "Drawable.h"
+#include "../Colour.h"
+
+class Text : public Drawable {
+public:
+    enum Alignment {
+        LEFT, CENTER, RIGHT
+    };
+
+    Text(std::string text, sf::Font& font, int size);
+    Text(sf::Font& font, int size);
+
+    void set_text(std::string text);
+    std::string get_text();
+
+    sf::Font get_font();
+    int get_size();
+
+    void set_colour(Colour colour);
+    void set_border(int size, Colour colour);
+    void set_alignment(Alignment alignment);
+
+    void draw(float x, float y, Window* window);
+
+    static sf::Font load_font(std::string location);
+private:
+    std::string text;
+    sf::Font font;
+    int size;
+    Alignment alignment;
+
+    sf::Text text_sf;
+    float render_offset_x = 0;
+
+    void update_text_position();
+};
+
+#endif
